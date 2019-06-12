56efca20b2248916fc17eba7d0407926eacbc6d7
Uli Köhler <ulikoehler@online.de>
2019-06-12 16:34:28 +0200

    Added SurfaceFromFace

--- inc/SurfaceUtils.hxx
+++ inc/SurfaceUtils.hxx
@@ -1,5 +1,8 @@
 #pragma once
 #include <TopoDS_Shape.hxx>
+#include <TopoDS_Face.hxx>
+#include <GeomAdaptor_Surface.hxx>
+#include <optional>
 
 namespace OCCUtil {
     namespace SurfaceUtils {
@@ -7,5 +10,10 @@ namespace OCCUtil {
          * Get the total surface area of a face, solid etc.
          */
         double SurfaceArea(const TopoDS_Shape& face);
+        /**
+         * Get the 3D surface from a given face
+         */
+        std::optional<GeomAdaptor_Surface> SurfaceFromFace(const TopoDS_Face& face);
+
     }
 }
--- src/SurfaceUtils.cxx
+++ src/SurfaceUtils.cxx
@@ -1,9 +1,20 @@
 #include "SurfaceUtils.hxx"
 #include <GProp_GProps.hxx>
 #include <BRepGProp.hxx>
+#include <BRepLib_FindSurface.hxx>
+
+using namespace std;
 
 double SurfaceArea(const TopoDS_Shape& face) {
     GProp_GProps gprops;
     BRepGProp::SurfaceProperties(face, gprops);
     return gprops.Mass();
+}
+
+std::optional<GeomAdaptor_Surface> SurfaceFromFace(const TopoDS_Face& face) {
+    BRepLib_FindSurface bfs(face);
+    if (!bfs.Found()) {
+        return std::nullopt;
+    }
+    return make_optional(bfs.Surface());
 }
