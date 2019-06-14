2d2f6f55bd936ed48a4fcf0be63026f18d5cd0d1
Uli Köhler <ulikoehler@online.de>
2019-06-14 06:30:07 +0200

    Added adaptive radius filleting

--- include/occutils/Fillet.hxx
+++ include/occutils/Fillet.hxx
@@ -1,5 +1,7 @@
 #pragma once
 #include <TopoDS_Shape.hxx>
+#include <TopoDS_Edge.hxx>
+#include <functional>
 
 namespace OCCUtils {
     namespace Fillet {
@@ -10,5 +12,19 @@ namespace OCCUtils {
          * Usually the shape is a solid.
          */
         TopoDS_Shape FilletAll(const TopoDS_Shape& shape, double radius=1.0);
+
+
+        /**
+         * Fillet all edges of the given shape using a
+         * user-defined function to define the radius separately
+         * for each edge.
+         * 
+         * radiusByEdge takes the edge and should return either:
+         *  - A radius
+         *  - or NaN if this edge should NOT be filleted
+         *
+         * Usually the shape is a solid.
+         */
+        TopoDS_Shape FilletAdaptiveRadius(const TopoDS_Shape& shape, std::function<double(const TopoDS_Edge&)> radiusByEdge);
     }
 }
--- src/Fillet.cxx
+++ src/Fillet.cxx
@@ -4,6 +4,7 @@
 #include <TopTools_IndexedMapOfShape.hxx>
 #include <TopExp.hxx>
 #include <TopoDS.hxx>
+#include <numeric>
 
 TopoDS_Shape OCCUtils::Fillet::FilletAll(const TopoDS_Shape& shape, double radius) {
     
@@ -15,6 +16,24 @@ TopoDS_Shape OCCUtils::Fillet::FilletAll(const TopoDS_Shape& shape, double radiu
     for (size_t i = 1; i <= edges.Extent(); i++) {
         const TopoDS_Edge& edge = TopoDS::Edge(edges(i));
         filletMaker.Add(radius, edge);
+    }
+    filletMaker.Build();
+    return filletMaker.Shape();
+}
+
+TopoDS_Shape OCCUtils::Fillet::FilletAdaptiveRadius(const TopoDS_Shape& shape, std::function<double(const TopoDS_Edge&)> radiusByEdge) {
+    
+    BRepFilletAPI_MakeFillet filletMaker(shape);
+    // Iterate edges
+    TopTools_IndexedMapOfShape edges;
+    TopExp::MapShapes (shape, TopAbs_EDGE, edges);
+
+    for (size_t i = 1; i <= edges.Extent(); i++) {
+        const TopoDS_Edge& edge = TopoDS::Edge(edges(i));
+        double radius = radiusByEdge(edge);
+        if(!std::isnan(radius)) { // NaN => dont fillet this edge!
+            filletMaker.Add(radius, edge);
+        }
     }
     filletMaker.Build();
     return filletMaker.Shape();
