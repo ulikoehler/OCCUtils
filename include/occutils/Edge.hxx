ca5ba94a4ee0091f2e1530aa2980f5fc626f953a
Uli Köhler <ulikoehler@online.de>
2019-06-15 02:49:23 +0200

    Added basic edge building utility

---
+++ include/occutils/Edge.hxx
@@ -0,0 +1,12 @@
+#pragma once
+#include <TopoDS_Edge.hxx>
+#include <gp_Pnt.hxx>
+
+namespace OCCUtils {
+    namespace Edge {
+        /**
+         * Create a linear edge between two points
+         */
+        TopoDS_Edge MakeEdge (const gp_Pnt& p1, const gp_Pnt& p2);
+    }
+}
---
+++ src/Edge.cxx
@@ -0,0 +1,13 @@
+#include "occutils/Edge.hxx"
+#include <Precision.hxx>
+#include <BRepBuilderAPI_MakeEdge.hxx>
+
+TopoDS_Edge OCCUtils::Edge::MakeEdge (const gp_Pnt& p1, const gp_Pnt& p2) {
+    // Are the two points the same?
+    // If so, return an "empty" edge
+    if (p1.Distance(p2) <= Precision::Confusion ()) {
+        return TopoDS_Edge ();
+    }
+    // Not the same => Create a linear edge
+    return BRepBuilderAPI_MakeEdge(p1, p2).Edge();
+}
