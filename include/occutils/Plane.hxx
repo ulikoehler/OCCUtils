4bb205460c99816679fb315a8860fb02e30e58f5
Uli Köhler <ulikoehler@online.de>
2019-06-15 05:36:35 +0200

    Improve docs

--- include/occutils/Plane.hxx
+++ include/occutils/Plane.hxx
@@ -10,6 +10,7 @@ namespace OCCUtils {
          *  - (U,V)(0,0) = pO
          *  - X axis = gp_Vec(pO, pX)
          *  - Y axis = gp_Vec(pO, pY)
+         * Precondition: No pair of points must coincide.
          * @throws OCCConstructionFailedException() if the parameters are invalid
          */
         gp_Pln FromPoints(const gp_Pnt& pO, const gp_Pnt& pX, const gp_Pnt& pY);
