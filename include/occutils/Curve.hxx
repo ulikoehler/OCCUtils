f4ed807fdd928d652ac9543da6513233a4f83e4b
Uli Köhler <ulikoehler@online.de>
2019-06-16 02:08:10 +0200

    Added more curve functions

--- include/occutils/Curve.hxx
+++ include/occutils/Curve.hxx
@@ -1,6 +1,7 @@
 #pragma once
 #include <GeomAdaptor_Curve.hxx>
 #include <Geom_Curve.hxx>
+#include <Geom_TrimmedCurve.hxx>
 #include <TopoDS_Edge.hxx>
 
 namespace OCCUtils {
@@ -10,5 +11,27 @@ namespace OCCUtils {
          * If it fails, returnValue.Curve().IsNull() == true
          */
         GeomAdaptor_Curve FromEdge(const TopoDS_Edge& edge);
+
+        /**
+         * Convert a trimmed curve to a GeomAdapter_Curve
+         */
+        GeomAdaptor_Curve FromTrimmedCurve(const Geom_TrimmedCurve &curve);
+
+        /**
+         * Length of a curve (by adaptor)
+         */
+        double Length(const GeomAdaptor_Curve& curve);
+        /**
+         * Length of a curve (by curve handle).
+         * Note that many Geom_Curve subclasses describe infinite
+         * curves and Geom_Curve does not have the correct trimming
+         * parameters by itself. Use GeomAdaptor_Curve or
+         * Geom_TrimmedCurve instead.
+         */
+        double Length(const Handle(Geom_Curve)& curve);
+        /**
+         * Length of a curve (by curve handle)
+         */
+        double Length(const Geom_TrimmedCurve &curve);
     }
 }
--- src/Curve.cxx
+++ src/Curve.cxx
@@ -1,10 +1,29 @@
 #include "occutils/Curve.hxx"
 #include <BRep_Tool.hxx>
-
+#include <GCPnts_AbscissaPoint.hxx>
 
 GeomAdaptor_Curve OCCUtils::Curve::FromEdge(const TopoDS_Edge& edge) {
     Standard_Real umin, umax;
     // Get unbounded curve plus separate bounding parameters
     auto rawCurve = BRep_Tool::Curve(edge, umin, umax);
     return GeomAdaptor_Curve(rawCurve, umin, umax);
+}
+
+GeomAdaptor_Curve OCCUtils::Curve::FromTrimmedCurve(const Geom_TrimmedCurve &curve) {
+    return GeomAdaptor_Curve(
+            curve.BasisCurve(), curve.FirstParameter(), curve.LastParameter()
+    );
+}
+    
+
+double OCCUtils::Curve::Length(const GeomAdaptor_Curve& curve) {
+    return GCPnts_AbscissaPoint::Length(curve);
+}
+
+double OCCUtils::Curve::Length(const Handle(Geom_Curve)& curve) {
+    return Length(GeomAdaptor_Curve(curve));
+}
+
+double OCCUtils::Curve::Length(const Geom_TrimmedCurve &curve) {
+    return Length(Curve::FromTrimmedCurve(curve));
 }
