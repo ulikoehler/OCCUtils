af94b88b4a0003476a35f2d911205974ce0b8144
Uli Köhler <ulikoehler@online.de>
2019-06-25 22:16:54 +0200

    Added surface filter functions

--- include/occutils/Surface.hxx
+++ include/occutils/Surface.hxx
@@ -5,6 +5,8 @@
 #include <TopoDS_Shape.hxx>
 #include <TopoDS_Face.hxx>
 #include <GeomAdaptor_Surface.hxx>
+#include <vector>
+#include <functional>
 
 namespace OCCUtils {
     namespace Surface {
@@ -27,5 +29,41 @@ namespace OCCUtils {
          * This is more efficient than computing them individually.
          */
         std::pair<double, gp_Pnt> AreaAndCenterOfMass(const TopoDS_Shape& face);
+
+        bool IsPlane(const GeomAdaptor_Surface& surf);
+        bool IsCylinder(const GeomAdaptor_Surface& surf);
+        bool IsCone(const GeomAdaptor_Surface& surf);
+        bool IsSphere(const GeomAdaptor_Surface& surf);
+        bool IsTorus(const GeomAdaptor_Surface& surf);
+        bool IsBezierSurface(const GeomAdaptor_Surface& surf);
+        bool IsBSplineSurface(const GeomAdaptor_Surface& surf);
+        bool IsSurfaceOfRevolution(const GeomAdaptor_Surface& surf);
+        bool IsSurfaceOfExtrusion(const GeomAdaptor_Surface& surf);
+        bool IsOffsetSurface(const GeomAdaptor_Surface& surf);
+        bool IsOtherSurface(const GeomAdaptor_Surface& surf);
+    }
+    namespace Surfaces {
+        struct SurfaceInfo {
+            TopoDS_Face face;
+            GeomAdaptor_Surface surface;
+        };
+        /**
+         * Get all faces & their surfaces that are within shape.
+         * Uses ShapeComponents::AllFacesWithin(shape) internally.
+         * Does not return surfaces that have no surface   
+         */
+        std::vector<SurfaceInfo> FromShape(const TopoDS_Shape& shape);
+
+        /**
+         * Filter surfaces by type
+         */
+        std::vector<SurfaceInfo> Filter(const std::vector<SurfaceInfo>& surfaces,
+            GeomAbs_SurfaceType type);
+
+        /**
+         * Filter surfaces by a custom filter function
+         */
+        std::vector<SurfaceInfo> Filter(const std::vector<SurfaceInfo>& surfaces,
+            const std::function<bool(const GeomAdaptor_Surface& surf)>& filt);
     }
 }
--- src/Surface.cxx
+++ src/Surface.cxx
@@ -2,6 +2,8 @@
 #include <GProp_GProps.hxx>
 #include <BRepGProp.hxx>
 #include <BRepLib_FindSurface.hxx>
+#include <algorithm>
+#include "occutils/ShapeComponents.hxx"
 
 using namespace std;
 
@@ -29,4 +31,89 @@ std::pair<double, gp_Pnt> OCCUtils::Surface::AreaAndCenterOfMass(const TopoDS_Sh
     GProp_GProps gprops;
     BRepGProp::SurfaceProperties(face, gprops);
     return std::make_pair(gprops.Mass(), gprops.CentreOfMass());
+}
+
+
+std::vector<OCCUtils::Surfaces::SurfaceInfo> OCCUtils::Surfaces::FromShape(const TopoDS_Shape& shape) {
+    auto faces = ShapeComponents::AllFacesWithin(shape);
+    // Create return vector
+    std::vector<OCCUtils::Surfaces::SurfaceInfo> ret;
+    ret.reserve(faces.size());
+    for(const auto& face : faces) {
+        SurfaceInfo info;
+        info.face = face;
+        info.surface = Surface::FromFace(face);
+        if(info.surface.Surface().IsNull()) { // If we have found the surface
+            ret.push_back(info); 
+        }
+    }
+    return ret;
+}
+
+
+bool OCCUtils::Surface::IsPlane(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_Plane;
+}
+
+bool OCCUtils::Surface::IsCylinder(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_Cylinder;
+}
+
+bool OCCUtils::Surface::IsCone(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_Cone;
+}
+
+bool OCCUtils::Surface::IsSphere(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_Sphere;
+}
+
+bool OCCUtils::Surface::IsTorus(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_Torus;
+}
+
+bool OCCUtils::Surface::IsBezierSurface(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_BezierSurface;
+}
+
+bool OCCUtils::Surface::IsBSplineSurface(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_BSplineSurface;
+}
+
+bool OCCUtils::Surface::IsSurfaceOfRevolution(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_SurfaceOfRevolution;
+}
+
+bool OCCUtils::Surface::IsSurfaceOfExtrusion(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_SurfaceOfExtrusion;
+}
+
+bool OCCUtils::Surface::IsOffsetSurface(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_OffsetSurface;
+}
+
+bool OCCUtils::Surface::IsOtherSurface(const GeomAdaptor_Surface& surf) {
+    return surf.GetType() == GeomAbs_OtherSurface;
+}
+
+
+std::vector<OCCUtils::Surfaces::SurfaceInfo> OCCUtils::Surfaces::Filter(const std::vector<SurfaceInfo>& surfaces,
+    GeomAbs_SurfaceType type) {
+    return Filter(surfaces, [&](const GeomAdaptor_Surface& surf) {
+        return surf.GetType() == type;
+    });
+};
+
+/**
+ * Filter surfaces by a custom filter function
+ */
+std::vector<OCCUtils::Surfaces::SurfaceInfo> OCCUtils::Surfaces::Filter(const std::vector<SurfaceInfo>& surfaces,
+    const std::function<bool(const GeomAdaptor_Surface& surf)>& filt) {
+    // Create output vector
+    std::vector<SurfaceInfo> ret;
+    ret.reserve(surfaces.size());
+    // Run algorithm
+    std::copy_if(surfaces.begin(), surfaces.end(), std::back_inserter(ret), [&](const SurfaceInfo& surf) {
+        return filt(surf.surface);
+    });
+    return ret;
 }
