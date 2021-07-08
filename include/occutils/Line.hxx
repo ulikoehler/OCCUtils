77c7cb2880ee7c93dc84de81d93d9237b9b5e5c6
Uli Köhler <ukoehler@techoverflow.net>
2021-07-08 18:15:08 +0200

    Added Line::Intersection

--- include/occutils/Line.hxx
+++ include/occutils/Line.hxx
@@ -4,6 +4,8 @@
  */
 #include <gp_Ax1.hxx>
 #include <gp_Lin.hxx>
+#include <gp_Lin2d.hxx>
+#include <optional>
 
 namespace OCCUtils {
     namespace Line {
@@ -18,5 +20,13 @@ namespace OCCUtils {
          * @return true if lin1 is parallel to lin2 to within angularTolerance
          */
         bool IsParallel(const gp_Lin &lin1, const gp_Lin &lin2, double angularTolerance=1e-6);
+
+
+        /**
+         * Comute the 2D intersection between two lines.
+         * @returns nullopt if there is no intersection or the Algorithm fails, the 2D point else
+         */
+        std::optional<gp_Pnt2d> Intersection(const gp_Lin2d &lin1, const gp_Lin2d &lin2);
+
     }
 }
--- src/Line.cxx
+++ src/Line.cxx
@@ -1,6 +1,7 @@
 #include "occutils/Line.hxx"
 #include <BRepAlgoAPI_Cut.hxx>
 #include <BRepAlgoAPI_Fuse.hxx>
+#include <IntAna2d_AnaIntersection.hxx>
 
 bool OCCUtils::Line::IsNormal(const gp_Lin &lin1, const gp_Lin &lin2, double angularTolerance) {
     // lin.Position() returns a gp_Ax1 that can be used to compute the normalness
@@ -9,4 +10,15 @@ bool OCCUtils::Line::IsNormal(const gp_Lin &lin1, const gp_Lin &lin2, double ang
 
 bool OCCUtils::Line::IsParallel(const gp_Lin &lin1, const gp_Lin &lin2, double angularTolerance) {   
     return lin1.Position().IsParallel(lin2.Position(), angularTolerance);
+}
+
+std::optional<gp_Pnt2d> OCCUtils::Line::Intersection(const gp_Lin2d &lin1, const gp_Lin2d &lin2) {
+    auto intersector = IntAna2d_AnaIntersection(lin1, lin2);
+    if(!intersector.IsDone()) { // Algorithm failure, returned as no intersection
+        return std::nullopt;
+    }
+    if (intersector.NbPoints() == 0) {
+        return std::nullopt;
+    }
+    return intersector.Point(0).Value();
 }
