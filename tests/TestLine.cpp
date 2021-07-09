841dc09cce852ff64967144f0a2f563b3e519b97
Uli Köhler <ukoehler@techoverflow.net>
2021-07-09 20:27:42 +0200

    More line intersection unit tests

--- tests/TestLine.cpp
+++ tests/TestLine.cpp
@@ -11,9 +11,23 @@ BOOST_AUTO_TEST_CASE( Line2D )
     gp_Lin2d lin1(gp_Pnt2d(0, 0), OCCUtils::Direction::X2d());
     auto result = OCCUtils::Line::Intersection(lin1, lin1);
     BOOST_CHECK(!result.has_value());
+
     // Two intersecting lines should have an intersection point
     gp_Lin2d lin2(gp_Pnt2d(0, -1), OCCUtils::Direction::Y2d());
     result = OCCUtils::Line::Intersection(lin1, lin2);
     BOOST_CHECK(result.has_value());
     BOOST_CHECK_EQUAL(result.value(), gp_Pnt2d(0, 0));
+
+    // Two more intersecting lines that do not intersect @ origin
+    gp_Lin2d lin3(gp_Pnt2d(0, 1), OCCUtils::Direction::X2d());
+    result = OCCUtils::Line::Intersection(lin2, lin3);
+    BOOST_CHECK(result.has_value());
+    BOOST_CHECK_EQUAL(result.value(), gp_Pnt2d(0, 1));
+
+    // ... same as last test but argument order inverted (same result)
+    result = OCCUtils::Line::Intersection(lin3, lin2);
+    BOOST_CHECK(result.has_value());
+    BOOST_CHECK_EQUAL(result.value(), gp_Pnt2d(0, 1));
+
+
 }
