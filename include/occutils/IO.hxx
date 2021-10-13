96ad7689eba6f4c845c62d7cab17ec435899b3d5
Uli Köhler <ukoehler@techoverflow.net>
2021-10-14 00:51:25 +0200

    Started to add higher level read functions

--- include/occutils/IO.hxx
+++ include/occutils/IO.hxx
@@ -1,10 +1,19 @@
 #pragma once
+#include <XSControl_Reader.hxx>
 #include <TopoDS_Shape.hxx>
 #include <string>
 #include <memory>
 
 namespace OCCUtils {
     namespace IO {
+        /**
+         * Get the appropriate XSControl_Reader instance for the given filename.
+         * - If filename ends with ".stp" or ".step" (case-insensitive), it will use STEPControl_Reader()
+         * - If filename ends with ".iges" or ".igs" (case-insensitive), it will use IGESControl_Reader()
+         */
+        TopoDS_Shape Read(const std::string& filename);
+
+
         namespace Reader {
             /**
              * Get the appropriate XSControl_Reader instance for the given filename.
--- src/IO.cxx
+++ src/IO.cxx
@@ -1,22 +1,23 @@
 #include "occutils/IO.hxx"
 #include "occutils/Exceptions.hxx"
 
-#include <XSControl_Reader.hxx>
+#include <STEPControl_Reader.hxx>
+#include <IGESControl_Reader.hxx>
 #include <boost/algorithm/string.hpp>
 
 using namespace std;
 using namespace boost::algorithm;
 
-shared_ptr<XSControl_Reader> OCCUtils::IO::Reader::STEPorIGESReader(const std::string& filename) {
+std::shared_ptr<XSControl_Reader> OCCUtils::IO::Reader::STEPorIGESReader(const std::string& filename) {
     shared_ptr<XSControl_Reader> reader;
     // Automatically determine filename
-    string lowercaseFilename = to_lower_copy(stepFilename);
+    string lowercaseFilename = to_lower_copy(filename);
     if (ends_with(lowercaseFilename, ".step") || ends_with(lowercaseFilename, ".stp")) {
         reader = shared_ptr<XSControl_Reader>(dynamic_cast<XSControl_Reader*>(new STEPControl_Reader()));
     } else if (ends_with(lowercaseFilename, ".iges") || ends_with(lowercaseFilename, ".igs")) {
         reader = shared_ptr<XSControl_Reader>(dynamic_cast<XSControl_Reader*>(new IGESControl_Reader()));
     } else {
-        throw OCCIOException("Unknown file extension (.stp/.step or .igs/.iges expected): " + stepFilename);
+        throw OCCIOException("Unknown file extension (.stp/.step or .igs/.iges expected): " + filename);
     }
     return reader;
 }
